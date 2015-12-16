a247b21d44db1d6f0eb3fba9aefb965bece72cbe
Emily Bowe <emilyrbowe@gmail.com>
2015-12-16 11:21:59 -0600

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name randdirApp
+ * @description
+ * # randdirApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('randdirApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randdirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the randdirApp
+ */
+angular.module('randdirApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
