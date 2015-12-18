fea5883312affb5c72fd580fc3051b176bd4405a
Emily Bowe <emilyrbowe@gmail.com>
2015-12-18 12:53:06 -0600

    Floor text updates in MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,61 @@
  * Controller of the randdirApp
  */
 angular.module('randdirApp')
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.floors = [
       {
-        id: 123,
-        floorNumber: 1,
-        landlord: "Weston Urban"
+        id: 100,
+        floorNumber: 0,
+        floorName: 'Basement',
+        landlord: 'Weston Urban'
       },
       {
-        id: 345,
+        id: 101,
+        floorNumber: 1,
+        floorName: '1st Floor',
+        landlord: 'Weston Urban'
+      },
+      {
+        id: 102,
         floorNumber: 2,
-        landlord: "Geekdom"
+        floorName: '2nd Floor',
+        landlord: 'Weston Urban'
+      },
+      {
+        id: 103,
+        floorNumber: 3,
+        floorName: '3rd Floor',
+        landlord: 'Weston Urban'
+      },
+      {
+        id: 104,
+        floorNumber: 4,
+        floorName: '4th Floor',
+        landlord: 'Weston Urban'
+      },
+      {
+        id: 105,
+        floorNumber: 5,
+        floorName: '5th Floor',
+        landlord: 'Weston Urban'
+      },
+      {
+        id: 106,
+        floorNumber: 6,
+        floorName: '6th Floor',
+        landlord: 'Geekdom'
+      },
+      {
+        id: 107,
+        floorNumber: 7,
+        floorName: '7th Floor',
+        landlord: 'Geekdom'
+      },
+      {
+        id: 108,
+        floorNumber: 8,
+        floorName: '8th Floor',
+        landlord: 'Geekdom'
       }
     ];
   }]);
