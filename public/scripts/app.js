f060e8679f8a5c81fcfb63b2acba49baeeb089be
Emily Bowe <emilyrbowe@gmail.com>
2016-01-19 16:45:44 -0600

    Added public files

---
+++ public/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name randdirApp
+ * @description
+ * # randdirApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('randdirApp', [
+    'firebase',
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ public/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randdirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the randdirApp
+ */
+angular.module('randdirApp')
+  .controller('MainCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function ($scope, $firebaseObject, $firebaseArray) {
+    var fbRef = new Firebase('https://rand2.firebaseio.com/');
+    var floorsRef = fbRef.child('floors');
+    $scope.floors = $firebaseArray(floorsRef);
+
+
+  }]);
