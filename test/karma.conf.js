dff38b654fbba43ee5d2a8dadffa78e20025dd64
Emily Bowe <emilyrbowe@gmail.com>
2016-01-21 17:34:28 -0600

    tenant ordering and styling changes via tim

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/firebase/firebase.js',
       'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/underscore/underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
