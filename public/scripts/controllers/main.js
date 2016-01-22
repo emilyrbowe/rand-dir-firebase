5b29bce1f2a22e21ce373784e3bf35d2c032850e
Emily Bowe <emilyrbowe@gmail.com>
2016-01-22 14:56:54 -0600

    Updating CSS, directory JSON

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -16,10 +16,10 @@ angular.module('randdirApp')
 
   }]);
 
-  angular.module('randdirApp')
-    .filter('toArray', function() { return function(obj) {
-      if (!(obj instanceof Object)) return obj;
-      return _.map(obj, function(val, key) {
-          return Object.defineProperty(val, '$key', {__proto__: null, value: key});
-      });
-    }});
+angular.module('randdirApp')
+  .filter('toArray', function() { return function(obj) {
+    if (!(obj instanceof Object)) return obj;
+    return _.map(obj, function(val, key) {
+        return Object.defineProperty(val, '$key', {__proto__: null, value: key});
+    });
+  }});
